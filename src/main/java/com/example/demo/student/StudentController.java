cb576e0837577e306ae13801f3e27a0ab5c0b38b
Caleb <calebrodgz@gmail.com>
2023-03-16 19:09:25 -0500

    Setup Spring Boot using Spring Initializr with some dependencies, Comment out the spring boot starter data jpa for now, Created Student, StudentController, and StudentService Classes, generated constructors, getters, and setters, listing Student properties and using @RequestMapping in StudentController Class

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService; // Reference
+
+    public StudentController(StudentService studentService) { // Constructor
+        this.studentService = studentService;
+    }
+
+    @GetMapping // annotate with @GetMapping to serve the method as a RESTful endpoint at "/". The route is in the parenthesis, also can pass parameters like method type and rout name itself
+    public List<Student> getStudents() { // This method returns a JSON list
+        return studentService.getStudents();
+    }
+}
